fcca588fc94a6d54bb5e7f911b4f701cbf6f93c3
MJERIBI <Mohamed-Allala.JERIBI@ingenico.com>
2024-03-27 12:03:22 +0100

    add src

--- src/main/java/epaytest_automated/Calculator.java
+++ src/main/java/epaytest_automated/Calculator.java
@@ -4,118 +4,296 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Calculator {
-	
+
 	public int add(int num1, int num2) {
 		return num1 + num2;
 	}
-	
+
 
 	public int add(int num1, int num2, int num3) {
 		return num1 + num2 + num3;
 	}
-	
+
 	public int subtract(int num1, int num2, int num3) {
 		return num1 - num2 - num3;
 	}
-	
+
 	public int subtract(int num1, int num2) {
 		return num1 - num2;
 	}
-	
+
 	public  int ouverture_adb() {
+		try {
+			// Commande pour ouvrir l'invite de commande et exécuter "adb devices" dans Windows
+			String[] command = {"cmd", "/c", "start", "adb", "devices"};
+
+			// Créer un ProcessBuilder avec la commande spécifiée
+			ProcessBuilder processBuilder = new ProcessBuilder(command);
+
+			// Démarrer le processus
+			processBuilder.start();
+
+			System.out.println("Invite de commande ouverte avec succès et commande adb devices exécutée.");
+			return 0;
+		} catch (IOException e) {
+			// Gérer l'exception en cas d'erreur lors de l'ouverture de l'invite de commande ou de l'exécution de adb
+			System.err.println("Erreur lors de l'ouverture de l'invite de commande ou de l'exécution de adb : " + e.getMessage());
+			e.printStackTrace();
+			return -2;
+		}
+	}
+
+
+	public  int ouverture_adb_sans_pop_up() {
+
+		try {
+			// Commande pour ouvrir l'invite de commande en arrière-plan dans Windows
+			String[] command = {"cmd", "/c"};
+
+			// Créer un ProcessBuilder avec la commande spécifiée
+			ProcessBuilder processBuilder = new ProcessBuilder(command);
+
+			// Rediriger la sortie et l'erreur standard
+			processBuilder.redirectOutput(ProcessBuilder.Redirect.INHERIT);
+			processBuilder.redirectError(ProcessBuilder.Redirect.INHERIT);
+
+			// Démarrer le processus
+			processBuilder.start();
+
+			System.out.println("Invite de commande ouverte en arrière-plan avec succès.");
+			return 0;
+		} catch (IOException e) {
+			// Gérer l'exception en cas d'erreur lors de l'ouverture de l'invite de commande
+			System.err.println("Erreur lors de l'ouverture de l'invite de commande : " + e.getMessage());
+			e.printStackTrace();
+			return -2;
+		}
+	}
+
+	public int  adb_devices_test() // deroule tous le test 
+	{
+		try {
+			// Commande pour ouvrir l'invite de commande et exécuter des commandes adb dans Windows
+			String[] command = {"cmd", "/c", "start", "adb", "devices", "&&", "adb", "root", "&&", "adb", "remount"};
+
+			// Créer un ProcessBuilder avec la commande spécifiée
+			ProcessBuilder processBuilder = new ProcessBuilder(command);
+
+			// Rediriger la sortie standard pour la lecture
+			processBuilder.redirectErrorStream(true);
+
+			// Démarrer le processus
+			Process process = processBuilder.start();
+
+			// Lire la sortie de la commande
+			try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+				String line;
+				System.out.println("Résultat de la commande adb shell epaytest --gtest_filter=IccardTest.psam1 :");
+				while ((line = reader.readLine()) != null) {
+					// Afficher chaque ligne de sortie
+					System.out.println(line);
+				}
+			}
+
+			// Attendre que le processus se termine
+			int exitCode = process.waitFor();
+
+			// Vérifier le code de sortie
+			if (exitCode != 0) {
+				// La commande s'est terminée avec un code de sortie non nul
+				System.err.println("La commande de sortie non nul : " + exitCode);
+				return -1;
+			} else {
+				System.out.println("La commande s'est exécutée avec succès.");
+				return 0;
+			}
+		} catch (IOException | InterruptedException e) {
+			// Gérer les exceptions en cas d'erreur lors de l'exécution de la commande adb shell
+			System.err.println("Erreur lors de l'exécution de la commande adb shell : " + e.getMessage());
+			e.printStackTrace();
+			return -1;
+		}
+
+
+
+	}
+
+
+
+	public int executeADBCommands() {
+		try {
+			// Commandes adb à exécuter
+			String[] adbCommands = {"adb devices", "adb root", "adb remount"};
+
+			// Créer un processus pour exécuter les commandes adb
+			ProcessBuilder processBuilder = new ProcessBuilder("cmd", "/c", "start");
+			for (String command : adbCommands) {
+				processBuilder.command().add(command);
+			}
+
+			Process process = processBuilder.start();
+
+			int exitCode = process.waitFor();
+
+			return exitCode;
+		} catch (IOException | InterruptedException e) {
+			e.printStackTrace();
+			return -1;
+		}
+	}
+
+	public int executeADBShellCommand(String adbShellCommand) {
+		try {
+
+			ProcessBuilder processBuilder = new ProcessBuilder("cmd", "/c", "start", "adb", "shell", adbShellCommand);
+
+
+			processBuilder.redirectErrorStream(true);
+
+
+			Process process = processBuilder.start();
+
+
+			try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+				String line;
+				System.out.println("Résultat de la commande adb shell " + adbShellCommand + ":");
+				while ((line = reader.readLine()) != null) {
+
+					System.out.println(line);
+				}
+			}
+
+
+			int exitCode = process.waitFor();
+
+
+			if (exitCode != 0) {
+
+				System.err.println("La commande adb shell " + adbShellCommand + " s'est terminée avec un code de sortie non nul : " + exitCode);
+			} else {
+				System.out.println("La commande adb shell " + adbShellCommand + " s'est exécutée avec succès.");
+			}
+
+			return exitCode;
+		} catch (IOException | InterruptedException e) {
+
+			e.printStackTrace();
+			return -1;
+		}
+	}
+
+
+	public int runTest3(String testName) {
+		try {
+			// Ouvrir une invite de commande et afficher le premier message
+			ProcessBuilder processBuilder = new ProcessBuilder("cmd", "/c", "start", "cmd", "/k", "echo Test en cours : " + testName + " && echo Insérer carte && pause");
+			processBuilder.inheritIO(); // Permet à l'utilisateur d'interagir avec l'invite de commande
+			processBuilder.start().waitFor(); // Attendre la fin de l'invite de commande
+			return 0;
+		} catch (IOException | InterruptedException e) {
+			e.printStackTrace();
+			return -1;
+		}
+	}
+
+
+
+	public int  adb_test()
+	{
+		try {
+			// Commande pour ouvrir l'invite de commande et exécuter des commandes adb dans Windows
+			String[] command = {"cmd", "/c", "start", "adb", "devices", "&&", "adb", "root", "&&", "adb", "remount", "&&", "adb", "shell", "epaytest", "--gtest_filter=IccardTest.smartcard"};
+
+			// Créer un ProcessBuilder avec la commande spécifiée
+			ProcessBuilder processBuilder = new ProcessBuilder(command);
+
+			// Rediriger la sortie standard pour la lecture
+			processBuilder.redirectErrorStream(true);
+
+			// Démarrer le processus
+			Process process = processBuilder.start();
+
+			// Lire la sortie de la commande
+			try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+				String line;
+				System.out.println("Résultat de la commande adb shell epaytest --gtest_filter=IccardTest.psam1 :");
+				while ((line = reader.readLine()) != null) {
+					// Afficher chaque ligne de sortie
+					System.out.println(line);
+				}
+			}
+
+			// Attendre que le processus se termine
+			int exitCode = process.waitFor();
+
+			// Vérifier le code de sortie
+			if (exitCode != 0) {
+				// La commande s'est terminée avec un code de sortie non nul
+				System.err.println("La commande adb shell epaytest --gtest_filter=IccardTest.psam1 s'est terminée avec un code de sortie non nul : " + exitCode);
+				return -1;
+			} else {
+				System.out.println("La commande adb shell epaytest --gtest_filter=IccardTest.psam1 s'est exécutée avec succès.");
+				return 0;
+			}
+		} catch (IOException | InterruptedException e) {
+			// Gérer les exceptions en cas d'erreur lors de l'exécution de la commande adb shell
+			System.err.println("Erreur lors de l'exécution de la commande adb shell : " + e.getMessage());
+			e.printStackTrace();
+			return -1;
+		}
+
+	}
+
+	
+	
+    public int adb_test2(String testName) {
         try {
-            // Commande pour ouvrir l'invite de commande et exécuter "adb devices" dans Windows
-            String[] command = {"cmd", "/c", "start", "adb", "devices"};
+            // Commande pour ouvrir l'invite de commande et exécuter des commandes adb dans Windows
+            String[] command = {"cmd", "/c", "start", "adb", "devices", "&&", "adb", "root", "&&", "adb", "remount", "&&", "adb", "shell", "epaytest", "--gtest_filter=" + testName};
 
             // Créer un ProcessBuilder avec la commande spécifiée
             ProcessBuilder processBuilder = new ProcessBuilder(command);
 
+            // Rediriger la sortie standard pour la lecture
+            processBuilder.redirectErrorStream(true);
+
             // Démarrer le processus
-            processBuilder.start();
+            Process process = processBuilder.start();
 
-            System.out.println("Invite de commande ouverte avec succès et commande adb devices exécutée.");
-            return 0;
-        } catch (IOException e) {
-            // Gérer l'exception en cas d'erreur lors de l'ouverture de l'invite de commande ou de l'exécution de adb
-            System.err.println("Erreur lors de l'ouverture de l'invite de commande ou de l'exécution de adb : " + e.getMessage());
-            e.printStackTrace();
-            return -2;
-        }
-    }
-  
-
-    public  int ouverture_adb_sans_pop_up() {
-   
-            try {
-                // Commande pour ouvrir l'invite de commande en arrière-plan dans Windows
-                String[] command = {"cmd", "/c"};
-
-                // Créer un ProcessBuilder avec la commande spécifiée
-                ProcessBuilder processBuilder = new ProcessBuilder(command);
-
-                // Rediriger la sortie et l'erreur standard
-                processBuilder.redirectOutput(ProcessBuilder.Redirect.INHERIT);
-                processBuilder.redirectError(ProcessBuilder.Redirect.INHERIT);
-
-                // Démarrer le processus
-                processBuilder.start();
-
-                System.out.println("Invite de commande ouverte en arrière-plan avec succès.");
-                return 0;
-            } catch (IOException e) {
-                // Gérer l'exception en cas d'erreur lors de l'ouverture de l'invite de commande
-                System.err.println("Erreur lors de l'ouverture de l'invite de commande : " + e.getMessage());
-                e.printStackTrace();
-                return -2;
+            // Lire la sortie de la commande
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+                String line;
+                System.out.println("Résultat de la commande adb shell epaytest --gtest_filter=" + testName + " :");
+                while ((line = reader.readLine()) != null) {
+                    
+                    System.out.println(line);
+                }
             }
+
+       
+            int exitCode = process.waitFor();
+
+           
+            if (exitCode != 0) {
+           
+                System.err.println("La commande adb shell epaytest --gtest_filter=" + testName + " s'est terminée avec un code de sortie non nul : " + exitCode);
+                return -1;
+            } else {
+                System.out.println("La commande adb shell epaytest --gtest_filter=" + testName + " s'est exécutée avec succès.");
+                return 0;
+            }
+        } catch (IOException | InterruptedException e) {
+           
+            System.err.println("Erreur lors de l'exécution de la commande adb shell : " + e.getMessage());
+            e.printStackTrace();
+            return -1;
         }
-    
-    public int  adb_devices_test()
-    {
-    	 try {
-             // Commande pour ouvrir l'invite de commande et exécuter des commandes adb dans Windows
-             String[] command = {"cmd", "/c", "start", "adb", "devices", "&&", "adb", "root", "&&", "adb", "remount", "&&", "adb", "shell", "epaytest", "--gtest_filter=IccardTest.psam1"};
-
-             // Créer un ProcessBuilder avec la commande spécifiée
-             ProcessBuilder processBuilder = new ProcessBuilder(command);
-
-             // Rediriger la sortie standard pour la lecture
-             processBuilder.redirectErrorStream(true);
-
-             // Démarrer le processus
-             Process process = processBuilder.start();
-
-             // Lire la sortie de la commande
-             try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
-                 String line;
-                 System.out.println("Résultat de la commande adb shell epaytest --gtest_filter=IccardTest.psam1 :");
-                 while ((line = reader.readLine()) != null) {
-                     // Afficher chaque ligne de sortie
-                     System.out.println(line);
-                 }
-             }
-
-             // Attendre que le processus se termine
-             int exitCode = process.waitFor();
-
-             // Vérifier le code de sortie
-             if (exitCode != 0) {
-                 // La commande s'est terminée avec un code de sortie non nul
-                 System.err.println("La commande adb shell epaytest --gtest_filter=IccardTest.psam1 s'est terminée avec un code de sortie non nul : " + exitCode);
-                 return -1;
-             } else {
-                 System.out.println("La commande adb shell epaytest --gtest_filter=IccardTest.psam1 s'est exécutée avec succès.");
-                 return 0;
-             }
-         } catch (IOException | InterruptedException e) {
-             // Gérer les exceptions en cas d'erreur lors de l'exécution de la commande adb shell
-             System.err.println("Erreur lors de l'exécution de la commande adb shell : " + e.getMessage());
-             e.printStackTrace();
-             return -1;
-         }
-  
     }
-    
-    
-    }
+	
+	
 
+
+
+
+
+}
--- src/test/java/epaytest_automated/calculatorTest.java
+++ src/test/java/epaytest_automated/calculatorTest.java
@@ -12,35 +12,71 @@ public class calculatorTest {
 	public void setUp() {
 		calc = new Calculator();
 	}
-	
+
+	String[] command_smartcard= {"smartcard","psam1","psam2","psam3","psam4"};
 	@Test
-	public void addTwoNumbers() {
-		assertEquals(20, calc.add(10, 10));
-		
-	}
+	public void smartCardTest() { assertEquals(0, calc.adb_test2("IccardTest."+command_smartcard[0]));}
+
+	/* SAM_TEST*/
+	@Test 
+	public void sam1CardTest() { assertEquals(0, calc.adb_test2("IccardTest."+command_smartcard[1]));}
+	@Test 
+	public void sam2CardTest() { assertEquals(0, calc.adb_test2("IccardTest."+command_smartcard[2]));}
+	@Test 
+	public void sam3CardTest() { assertEquals(0, calc.adb_test2("IccardTest."+command_smartcard[3]));}
+	@Test 
+	public void sam4CardTest() { assertEquals(0, calc.adb_test2("IccardTest."+command_smartcard[4]));}
+	
+
+	String[] command_rfcard= {"poll"};
+	@Test 
+	public void rfcardTestPoll() { assertEquals(0, calc.adb_test2("ClessTest."+command_rfcard[0]));}
+
+	String[] command_lifecycle= {"getState","getWarning"};
+	@Test 
+	public void lifecycleTestsGetState() {assertEquals(0, calc.adb_test2("Lifecycle."+ command_lifecycle[0]));} // Liifecycle Get state
+
+	@Test 
+	public void lifecycleTestsGetCurrentTamperState() {assertEquals(0, calc.adb_test2("Lifecycle."+ command_lifecycle[1]));} // Liifecycle Get warning 
 
 	@Test
-	public void addThreeNumbers() {
-		assertEquals(20, calc.add(5, 5, 10));
-		
-	}
+	public void touchPanelAutoTest() {assertEquals(0, calc.adb_test2("TpTest.autotest"));} // Touch Panel test 
+	@Test
+	public void touchPanelTestNormal() {assertEquals(0, calc.adb_test2("TpTest.normal"));} // Touch Panel test 
+	
+	String[] command_crypto= {"rsa2048","aes","stress","rsa1024","tdes"}; //test RSA
 
 	@Test
-	public void subtractTwoNumbers() {
-		assertEquals(0, calc.subtract(10, 10));
-		
-	}
+	public void cryptographicTestRsa2048() {assertEquals(0, calc.adb_test2("AlgoTest."+command_crypto[0]));} //Test crypto rsa 20248
 
 	@Test
-	public void subtractThreeNumbers() {
-		assertEquals(0, calc.subtract(20, 10, 10));
-		
-	}
+	public void cryptographicTestAesEcb() {assertEquals(0, calc.adb_test2("AlgoTest."+command_crypto[1]));} //Test AES ECB
+
+	@Test
+	public void cryptographicStressTest() {assertEquals(0, calc.adb_test2("AlgoTest."+command_crypto[2]));} //Test STRESS TEST 
+	
+	@Test
+	public void cryptographicsRsaKeyGenTest() {assertEquals(0, calc.adb_test2("AlgoTest.*"));} //Test Key gen *
+	
+	@Test
+	public void cryptographicTestRsa1024() {assertEquals(0, calc.adb_test2("AlgoTest."+command_crypto[3]));} //Test crypto rsa 1024
+	
+	@Test
+	public void cryptographicTestTdes() {assertEquals(0, calc.adb_test2("AlgoTest."+command_crypto[4]));} //Test crypto tdes
+	
+	@Test
+	public void SwitchSeToRunModeTest() {assertEquals(0, calc.adb_test2("MiscTest.wakeup"));} //Test SWITCH SE TO RUN MODE
+	
+	@Test
+	public void SwitchSeToSleepModeTest() {assertEquals(0, calc.adb_test2("MiscTest.sleep"));} //Test SWITCH SE TO low mode 
+	
+	@Test
+	public void GetBatteryVoltageTest() {assertEquals(0, calc.adb_test2("MiscTest.coinVol"));} //Test get battery voltage
+	
+	@Test
+	public void GetSeDataTimeTest() {assertEquals(0, calc.adb_test2("MiscTest.rtc"));} //Test get data time 
 	
 	
 	@Test
-	public void adb_devices_test() {
-		assertEquals(0, calc.adb_devices_test());
-		
-	}
-}
+	public void GetSeVersionTest() {assertEquals(0, calc.adb_test2("MiscTest.version"));} //Test Version SE 
+	
