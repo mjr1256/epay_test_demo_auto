8e228cb595a3fd3c1114764ce7665c5e04859916
MJERIBI <Mohamed-Allala.JERIBI@ingenico.com>
2024-03-27 13:49:01 +0100

    git commit src

--- src/test/java/epaytest_automated/calculatorTest.java
+++ src/test/java/epaytest_automated/calculatorTest.java
@@ -74,6 +74,4 @@ public class calculatorTest {
 	public void GetBatteryVoltageTest() {assertEquals(0, calc.adb_test2("MiscTest.coinVol"));} //Test get battery voltage
 	@Test
 	public void GetSeDataTimeTest() {assertEquals(0, calc.adb_test2("MiscTest.rtc"));} //Test get data time 
-	@Test
-	public void GetSeVersionTest() {assertEquals(0, calc.adb_test2("MiscTest.version"));} //Test Version SE 
 }
